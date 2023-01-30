734930fd378f6f812bd9d6bef2cd1c000fce7a69
Baui <nicolai@bauknecht.email>
2023-01-30 12:58:36 +0100

    till lesson 6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Notes />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Notes />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import SendIcon from "@material-ui/icons/Send";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        className={classes.title}
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        className={classes.btn}
+        onClick={() => console.log("Click!")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
