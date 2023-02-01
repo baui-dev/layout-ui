d8feface0ad6917863c82ea4aa1ed4925ddf236b
Baui <nicolai@bauknecht.email>
2023-02-01 15:15:29 +0100

    tutorial done
    masonry

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import NoteCard from "../components/NoteCard";
+import Masonry from "react-masonry-css";
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -22,15 +23,26 @@ export default function Notes() {
     setNotes(newNotes);
   };
 
+  const breakpointColumnsObj = {
+    default: 4,
+    1500: 3,
+    1100: 2,
+    700: 1,
+  };
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map((note) => (
-          <Grid item key={note.id} xs={12} md={6} lg={3}>
+          <div item key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 }
